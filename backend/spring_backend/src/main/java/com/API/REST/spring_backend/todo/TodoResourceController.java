b4d98bde74f94f7fe66e8c6c963944fba785f80b
kalanaelapatha <38855998+kalanaelapatha@users.noreply.github.com>
2020-01-05 19:02:43 +0530

    backend part are Completed

--- backend/spring_backend/src/main/java/com/API/REST/spring_backend/todo/TodoResourceController.java
+++ backend/spring_backend/src/main/java/com/API/REST/spring_backend/todo/TodoResourceController.java
@@ -1,19 +1,24 @@
 package com.API.REST.spring_backend.todo;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.Servlet;
 import javax.xml.ws.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.API.REST.spring_backend.todo.Todo;
 
@@ -54,11 +59,30 @@ public class TodoResourceController {
 	}
 
 	// PUT
-	 @PutMapping("users/{username}/todos/{id}")
-	public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo ) {
-		
-			Todo todoUpdated =todoService.save(todo); 	
-		return new ResponseEntity<Todo>(todo,HttpStatus.OK);
-	}
+	@PutMapping("users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id,
+			@RequestBody Todo todo) {
 
+		Todo todoUpdated = todoService.save(todo);
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+	}
+	
+	//Post Mapping
+	
+	@PostMapping("users/{username}/todos")
+	public ResponseEntity<Void> createTodo(@PathVariable String username,@RequestBody Todo todo) {
+
+		Todo createTodo = todoService.save(todo);
+		
+		//Location
+		//Get Current Resource URL
+		//{id}
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+						.path("/{id}").buildAndExpand(createTodo.getId()).toUri();//In here get the created resouce
+		
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
 }
